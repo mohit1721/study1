d6021c10c4eb1b135473675ca5ff204e764d478c
mohit1721 <mohitmandal192@gmail.com>
2024-03-21 12:37:56 +0530

    correct apiConnector 3

--- Server/index.js
+++ Server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:["https://study1.onrender.com","http://localhost:3000","https://mystudynotion.vercel.app","https://mystudynotion.vercel.app/api/v1","https://study1-jlkmw7ckr-mohit1721s-projects.vercel.app"],  //FOR FRONTEND....VVI..to entertain frontend req.[[http://localhost:3000]]
+    origin:["http://localhost:3000","https://mystudynotion.vercel.app","https://study1-jlkmw7ckr-mohit1721s-projects.vercel.app"],  //FOR FRONTEND....VVI..to entertain frontend req.[[http://localhost:3000]]
     credentials: true,
   })
 );
