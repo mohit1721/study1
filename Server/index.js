44407600ecd911968074da5133ec8b2df58ead0e
mohit1721 <mohitmandal192@gmail.com>
2024-03-22 12:05:01 +0530

    Re

--- Server/index.js
+++ Server/index.js
@@ -24,8 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:"*", //FOR FRONTEND....VVI..to entertain frontend req.[[http://localhost:3000]] -->:["http://localhost:3000","https://mystudynotion.vercel.app","https://study1-jlkmw7ckr-mohit1721s-projects.vercel.app"], --------------------------["https://mystudynotion.vercel.app"]
-   methods: ["GET", "POST", "PUT", "DELETE"],
+    origin:"*", //FOR FRONTEND..//  methods: ["GET", "POST", "PUT", "DELETE"],..VVI..to entertain frontend req.[[http://localhost:3000]] -->:["http://localhost:3000","https://mystudynotion.vercel.app","https://study1-jlkmw7ckr-mohit1721s-projects.vercel.app"], --------------------------["https://mystudynotion.vercel.app"]  
     credentials: true,
   })
 );
