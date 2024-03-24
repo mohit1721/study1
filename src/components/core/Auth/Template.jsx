344f303024c162f5d35f4f47fe6d09947dd415c0
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2024-03-24 21:45:37 +0530

    Update Template.jsx

--- src/components/core/Auth/Template.jsx
+++ src/components/core/Auth/Template.jsx
@@ -26,7 +26,7 @@ function Template({ title, description1, description2, image, formType }) {
             </p>
             {formType === "signup" ? <SignupForm /> : <LoginForm />}
           </div>
-          <div className="relative mx-auto w-11/12 max-w-[450px] md:mx-0">
+          <div className="relative mt-6 mx-auto w-11/12 max-w-[450px] md:mx-0">
             <img
               src={frameImg}
               alt="Pattern"
