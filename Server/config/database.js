2e76158ee1c8533be04e6ebe1ddbcdcaeb8eb86c
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2024-03-24 17:23:44 +0530

    Update database.js with async-await

--- Server/config/database.js
+++ Server/config/database.js
@@ -3,15 +3,31 @@ require("dotenv").config();
 
 const { MONGODB_URL } = process.env;
 
-exports.connect = () => {
-	mongoose.connect(MONGODB_URL, {
-			useNewUrlparser: true,
-			useUnifiedTopology: true,
-		})
-		.then(console.log(`DB Connection Success`))
-		.catch((err) => {
-			console.log(`DB Connection Failed`);
-			console.log(err);
-			process.exit(1);
-		});
+// exports.connect = () => {
+// 	mongoose.connect(MONGODB_URL, {
+// 			useNewUrlparser: true,
+// 			useUnifiedTopology: true,
+// 		})
+// 		.then(console.log(`DB Connection Success`))
+// 		.catch((err) => {
+// 			console.log(`DB Connection Failed`);
+// 			console.log(err);
+// 			process.exit(1);
+// 		});
+// };
+
+// chatgpt
+exports.connect = async () => {
+    try {
+        await mongoose.connect(MONGODB_URL, {
+			keepAlive: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`DB Connection Success`);
+    } catch (err) {
+        console.log(`DB Connection Failed`);
+        console.log(err);
+        process.exit(1);
+    }
 };
